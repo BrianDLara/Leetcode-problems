0c60763b13b09468a8dd2106bcfd90be2fd994f4
Brian Lara <96392130+BrianDLara@users.noreply.github.com>
2023-08-29 13:33:52 -0400

    Time: 56 ms (49.26%), Space: 41.4 MB (91.28%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,80 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+// var plusOne = function(digits) {
+//     let num
+//     let last = digits.length - 1;
+//     console.log(digits[last])
+
+//      if (digits[last] === 9 && digits[last - 1] === 9 && digits[last - 2] === 9){
+//         digits.pop()
+//         digits.pop()
+//         digits.pop()
+//         digits.push(1,0,0,0)
+//     } else if (digits[last] === 9 && digits[last - 1] === 9){
+//         digits.pop()
+//         digits.pop()
+//         digits.push(1,0,0)
+//     } else if (digits[last] === 9 && digits.length === 1){
+//         digits.pop()
+//         digits.push(1,0)
+
+//     }else if (digits[last] === 9){
+//         num = digits[last]
+//         digits.pop()
+//         digits[last] = num + 1
+//         digits.push(0)
+
+//     } else{
+//         num = digits[last]
+//         digits.pop()
+//         digits.push(num + 1)
+//     }
+//     return digits
+// };
+
+// var plusOne = function(digits) {
+//     let carry = 1; // Start with a carry of 1
+//     let last = digits.length - 1;
+
+//     for (let i = last; i >= 0; i--) {
+//         let sum = digits[i] + carry;
+
+//         digits[i] = sum % 10; // Set the current digit
+//         carry = Math.floor(sum / 10); // Calculate carry for the next iteration
+
+//         if (carry === 0) {
+//             // No more carry, exit loop
+//             break;
+//         }
+//     }
+
+//     // If carry is still 1, it means we need to add a new digit at the beginning
+//     if (carry === 1) {
+//         digits.unshift(1);
+//     }
+
+//     return digits;
+// };
+
+// var plusOne = function(digits) {
+//     // join all numbers into one string
+//     let join = digits.join('')
+    
+//     // convert it into an integer and add 1 to the number
+//     let number = parseInt(join) + 1
+
+//     // return an array that has been converted back to a string and split
+//     return number.toString().split('')
+// };
+
+
+
+
+
+// simplified version
+var plusOne = function(digits) {
+    // return an array that has been converted back to a string and split
+    return (BigInt(digits.join('')) + BigInt(1)).toString().split('')
+};
