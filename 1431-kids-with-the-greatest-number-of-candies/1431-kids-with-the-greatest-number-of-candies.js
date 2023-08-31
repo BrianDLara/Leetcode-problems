2e5db157ab384ef190040a0bef02147bb5faca59
Brian Lara <96392130+BrianDLara@users.noreply.github.com>
2023-08-30 21:05:23 -0400

    Time: 59 ms (49.50%), Space: 42.2 MB (71.86%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -3,12 +3,42 @@
  * @param {number} extraCandies
  * @return {boolean[]}
  */
+//////////////////////////////////////////////
+////////////////// solution 1 ////////////////
+//////////////////////////////////////////////
+
+// var kidsWithCandies = function(candies, extraCandies) {
+//     let decending = candies.slice().sort((a, b) => b - a);    
+//     let highest = decending[0]
+//     let arr = []  
+//     for (let candy of candies){
+//         arr.push(candy + extraCandies >= highest ? true : false)
+//     }
+//     return arr
+// };
+
+//////////////////////////////////////////////
+////////////////// solution 2 ////////////////
+//////////////////////////////////////////////
+
 var kidsWithCandies = function(candies, extraCandies) {
-    let decending = candies.slice().sort((a, b) => b - a);    
-    let highest = decending[0]
-    let arr = []  
-    for (let candy of candies){
-        arr.push(candy + extraCandies >= highest ? true : false)
+    let arr = []
+    let booleans = []
+    let highest = 0
+    
+    for (let i = 0; i < candies.length; i++){
+        arr.push(candies[i] + extraCandies)
+        if (highest < candies[i]){
+            highest = candies[i]
+        }
     }
-    return arr
+    
+    for (let i = 0; i < candies.length; i++){
+        if (highest <= arr[i]){
+            booleans.push(true)
+        } else{
+            booleans.push(false)
+        }
+    }
+    return booleans
 };
