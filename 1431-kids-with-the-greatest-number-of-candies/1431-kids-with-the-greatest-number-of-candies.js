9175379c9988766d2324da32ada0793ccb199bd9
Brian Lara <96392130+BrianDLara@users.noreply.github.com>
2023-08-30 21:00:37 -0400

    Time: 54 ms (73.29%), Space: 43.2 MB (9.26%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -4,22 +4,11 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-    let arr = []
-    let booleans = []
-    let highest = 0
-    for (let i = 0; i < candies.length; i++){
-        arr.push(candies[i] + extraCandies)
-        if (highest < candies[i]){
-            highest = candies[i]
-        }
+    let decending = candies.slice().sort((a, b) => b - a);    
+    let highest = decending[0]
+    let arr = []  
+    for (let candy of candies){
+        arr.push(candy + extraCandies >= highest ? true : false)
     }
-    
-    for (let i = 0; i < candies.length; i++){
-        if (highest <= arr[i]){
-            booleans.push(true)
-        } else{
-            booleans.push(false)
-        }
-    }
-    return booleans
+    return arr
 };
