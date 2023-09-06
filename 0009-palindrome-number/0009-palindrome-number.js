f51cb4e45695135d760d60984ac5eed7960340ed
Brian Lara <96392130+BrianDLara@users.noreply.github.com>
2023-09-05 20:46:16 -0400

    Time: 135 ms (80.92%), Space: 51.7 MB (12.03%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,7 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
+
+    //Negative numbers cannot be palindromes because when you reverse the digits of a negative number, it         doesn't produce a valid number. 
+    //Example: -121 !== 121-
     if (x < 0) return false 
+    
     x = x.toString()
     let left = 0
     let right = x.length - 1
