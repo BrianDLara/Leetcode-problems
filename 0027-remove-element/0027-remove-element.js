7b5b344aa2b9497d7e7b077945b8c1c22c6f26eb
Brian Lara <96392130+BrianDLara@users.noreply.github.com>
2023-09-14 20:53:14 -0400

    Time: 59 ms (26.13%), Space: 42.5 MB (8.61%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,12 +4,11 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-   let counter = 0
-   for(num of nums){
-       if (num !== val){
-           nums[counter] = num
-           counter++
-       }
-   }
-    return counter
+    for (let i = nums.length; i >= 0; i--){
+        if (nums[i] === val){
+            nums.splice(i, 1)
+        }
+    }
+    
+    return nums.length
 };
